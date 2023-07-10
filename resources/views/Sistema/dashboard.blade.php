f2e4b5bc5f7a9b7fb335734e2327ac743842e25f
XxDavexX <dave.santivanez@gmail.com>
2023-07-09 22:09:16 -0500

    avance

--- resources/views/Sistema/dashboard.blade.php
+++ resources/views/Sistema/dashboard.blade.php
@@ -9,11 +9,6 @@
     <header class="app-header">
       <nav class="navbar navbar-expand-lg navbar-light">
         <ul class="navbar-nav">
-          <li class="nav-item d-block d-xl-none">
-            <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
-                <i class='bx bxs-bell-ring'></i>
-            </a>
-          </li>
           <li class="nav-item">
             <a class="nav-link nav-icon-hover" href="javascript:void(0)">
                 <i class='bx bxs-bell-ring'></i>
